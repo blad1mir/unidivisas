896f6f3b96e989ea50cf61806d0889dc9a8c0624
Bladimir Aponte <bladimirapon@gmail.com>
2019-10-07 19:53:47 -0400

    servicio

--- src/app/Servicios/firestore.service.ts
+++ src/app/Servicios/firestore.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Solicitud } from '../modelos/interfaces';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
 
-  constructor() { }
+  Solicitudes: Observable<Solicitud[]>; 
+  SolicitudColeccion: AngularFirestoreCollection<Solicitud>;
+  solicitdDoc: AngularFirestoreDocument<Solicitud>;
+
+
+
+  constructor(public db: AngularFirestore, private http: HttpClient,  private router: Router) { }
+ 
+  getSolicitudes(){
+    this.SolicitudColeccion = this.db.collection('solicitudes');
+    this.Solicitudes = this.SolicitudColeccion.valueChanges();
+    return this.Solicitudes;
+
+  }
 }
---
+++ src/app/modelos/interfaces.ts
@@ -0,0 +1,8 @@
+
+export interface Solicitud { //interfaces utilizadas en todo el codigo
+    id?: string;
+    usuario?: string;
+    monto?: number;
+    tarifa?: number;
+    banco?: string;
+}
