38449561f9d740961e9ad06d123b70703ebe35a3
Bladimir Aponte <bladimirapon@gmail.com>
2019-10-07 09:01:45 -0400

    modulo de menu mas ventas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { VentaComponent } from './venta/venta.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    VentaComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
