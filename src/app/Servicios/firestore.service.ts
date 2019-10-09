e2ee25c08150717a89753521e0241912cb4b77a7
Bladimir Aponte <bladimirapon@gmail.com>
2019-10-09 09:29:29 -0400

    nb

--- src/app/Servicios/firestore.service.ts
+++ src/app/Servicios/firestore.service.ts
@@ -3,23 +3,23 @@ import { Observable } from 'rxjs';
 import { Solicitud } from '../modelos/interfaces';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
-import { HttpClient } from 'selenium-webdriver/http';
-
+import { HttpClient } from '@angular/common/http';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
-
-  Solicitudes: Observable<Solicitud[]>; 
-  SolicitudColeccion: AngularFirestoreCollection<Solicitud>;
+  Solicitudes: FirebaseListObservable<Solicitud[]>;
+  SolicitudColeccion: FirebaseObjectObservable<Solicitud>;
   solicitdDoc: AngularFirestoreDocument<Solicitud>;
 
 
 
   constructor(public db: AngularFirestore, private http: HttpClient,  private router: Router) { }
- 
-  getSolicitudes(){
-    this.SolicitudColeccion = this.db.collection('solicitudes');
+
+  getSolicitudes() {
+    this.SolicitudColeccion = this.db.collection('Solicitud');
     this.Solicitudes = this.SolicitudColeccion.valueChanges();
     return this.Solicitudes;
 
--- src/app/venta/venta.component.ts
+++ src/app/venta/venta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../Servicios/firestore.service';
 
 @Component({
   selector: 'app-venta',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentaComponent implements OnInit {
 
-  constructor() { }
+  solicitud = [];
+  constructor(private fs: FirestoreService) {
+    fs.getSolicitudes().subscribe(solicitud => {
+      this.solicitud = solicitud;
+    });
+
+   }
 
   ngOnInit() {
+
   }
 
 }
