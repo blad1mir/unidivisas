4491b67f52984c43914e00a17af8cc262897b552
nacho963 <46509023+nacho963@users.noreply.github.com>
2019-10-25 01:09:53 -0400

    TRANSACCIONES

--- src/app/transaccion/transaccion.component.ts
+++ src/app/transaccion/transaccion.component.ts
@@ -13,13 +13,19 @@ export class TransaccionComponent implements OnInit {
   constructor(private firestore: FirestoreService, private route: ActivatedRoute) { }
   public solicitud: Solicitud = {};
   public sumaa;
+  user="";
+  ListaBanco = [];
+  BancosPersonales= [];
    
   ngOnInit() {
     const idSolicitud = this.route.snapshot.params['id'];
     this.getDetailsSolicitud(idSolicitud);
+    this.obtenerListaBanco();
 
       var x = document.getElementById("aparecer");
       x.style.display="none";
+      var y = document.getElementById("desaparecer");
+      y.style.display="block";
   }
 
   
@@ -31,18 +37,40 @@ export class TransaccionComponent implements OnInit {
       this.sumaa=this.solicitud.monto*this.solicitud.tarifa;
     });
   }
-
+  obtenerListaBanco(){
+    this.BancosPersonales= [];
+    this.firestore.obtenerListaDeBanco()
+    .subscribe( ListaBanco =>{
+      this.ListaBanco = ListaBanco;
+      ListaBanco.forEach(elemento => {
+        if(elemento.usuario==this.solicitud.usuario && elemento.nombreBanco==this.solicitud.banco){
+          console.log(elemento.usuario)
+          console.log(this.user)
+          this.BancosPersonales.push(elemento);
+        }
+      })
+    })
+  
+  }
   
 
   confirmartransaccion(){
-    if(confirm('SEGURO QUE QUIERES REALIZAR ESTA TRANSACCIÓN')){
+    if(confirm('Seguro que quieres realizar esta transacción, no podra cancelar despues de este punto.')){
       var x = document.getElementById("aparecer");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
+
+    var y = document.getElementById("desaparecer");
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
     }
+    }
+    
 
 
     }
